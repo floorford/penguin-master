94cbd4a67845d20e232d4fb33d3ac92eaa6313c0
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-05-11 17:16:00 +0100

    added questions to quiz

--- js/quiz.js
+++ js/quiz.js
@@ -4,6 +4,8 @@
   let q2 = document.getElementById("question2");
   let q3 = document.getElementById("question3");
   let q4 = document.getElementById("question4");
+  let q5 = document.getElementById("question5");
+  let q6 = document.getElementById("question6");
 
   let p = document.createElement("p");
 
@@ -67,6 +69,38 @@
         p.textContent = "Wrong!";
         p.style.color = "red";
         q4.appendChild(p);
+      }
+    }
+  });
+
+  q5.addEventListener("click", event => {
+    let clicked = event.target;
+    if (clicked.tagName === "INPUT" ){
+      if (clicked.value === "correct" && clicked.checked) {
+        p.textContent = "Correct!";
+        p.style.color = "green";
+        q5.appendChild(p);
+      }
+      else {
+        p.textContent = "Wrong!";
+        p.style.color = "red";
+        q5.appendChild(p);
+      }
+    }
+  });
+
+  q6.addEventListener("click", event => {
+    let clicked = event.target;
+    if (clicked.tagName === "INPUT" ){
+      if (clicked.value === "correct" && clicked.checked) {
+        p.textContent = "Correct!";
+        p.style.color = "green";
+        q6.appendChild(p);
+      }
+      else {
+        p.textContent = "Wrong!";
+        p.style.color = "red";
+        q6.appendChild(p);
       }
     }
   });
